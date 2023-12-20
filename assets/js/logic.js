38f46a6b0507fb77294fdbe35a375fff4c0da783
Ryiong <viethung1209.work@gmail.com>
2023-12-20 22:02:01 +0700

    push code

---
+++ assets/js/logic.js
@@ -0,0 +1,34 @@
+// Display user infor when no infor
+const inforUser = document.getElementById('js-infor-user');
+if (inforUser.innerText == '') {
+    inforUser.style.display = "none";
+    console.log("delete infor user");
+}
+
+// Display logout
+const userBtn = document.querySelector('.link-user')
+const logoutBtn = document.querySelector('.logout-area')
+const fullWeb = document.querySelector('.full-web')
+function displayLogout(){
+    logoutBtn.style.display = "flex";
+    console.log('displayLogout')
+}
+function hideLogout(){
+    if (logoutBtn.style.display == "flex") {logoutBtn.style.display = null; console.log('hideLogout')
+    logoutBtn.addEventListener('click', function (event) {
+        event.stopPropagation()
+    })
+}
+}
+userBtn.addEventListener('click', displayLogout)
+userBtn.addEventListener('click', function (event) {
+    event.stopPropagation()
+})
+fullWeb.addEventListener('click', hideLogout)
+
+
+const tableItems = document.getElementsByClassName('mgn-content')
+for (let tableItem of tableItems) {
+    if (tableItem.innerText === 'F') {
+        tableItem.style.color = 'red'; tableItem.style.fontWeight = 'bold'
+    }}
